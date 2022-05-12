e0900da7f3867497524cb2ef72b90855e397e774
Matheus Proenca <matheusproencaz@gmail.com>
2022-05-12 13:44:56 -0300

    First Day - Netflix Clone

---
+++ README.md
@@ -0,0 +1,28 @@
+# NETFLIX WITH NEXT.JS AND TAILWIND
+
+**Projeto do canal no youtube ILW Yennefer**
+- **Técnologias utilizadas**:
+    - NEXT.JS <img style="height:25px;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nextjs-logo.svg/1280px-Nextjs-logo.svg.png"/> 
+    - Recoil <img style="height:25px;" src="https://recoiljs.org/img/logo.svg">
+    - TypeScript <img style="height:25px;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png" /> 
+    - Tailwind <img style="height:25px;" src="https://seeklogo.com/images/T/tailwind-css-logo-5AD4175897-seeklogo.com.png">
+    - Firebase <img style="height:25px;" src="https://firebase.google.com/downloads/brand-guidelines/PNG/logo-standard.png?hl=pt-br">
+    - Vercel <img style="height:25px;" src="https://www.svgrepo.com/show/354513/vercel-icon.svg">
+
+* **Assuntos abordados**:
+    - Consumo de REST APIs,
+    - CSS e Responsividade com Tailwind,
+    - Criação de componentes com o Next.JS,
+    - Server side rendering das imagens com o Next.JS,
+    - Deploy da aplicação no Vercel.
+    - ETC.
+
+
+
+
+
+## Deploy your own
+
+Deploy the example using [Vercel](https://vercel.com?utm_source=github&utm_medium=readme&utm_campaign=next-example) or preview live with [StackBlitz](https://stackblitz.com/github/vercel/next.js/tree/canary/examples/with-tailwindcss)
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https://github.com/vercel/next.js/tree/canary/examples/with-tailwindcss&project-name=with-tailwindcss&repository-name=with-tailwindcss)
---
+++ pages/index.tsx
@@ -0,0 +1,105 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+import requests from '../utils/requests'
+import { Movie } from '../typings'
+import Row from '../components/Row'
+
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+
+
+const Home = ({ 
+  netflixOriginals,
+  trendingNow,
+  topRated,
+  actionMovies,
+  comedyMovies,
+  horrorMovies,
+  romanceMovies,
+  documentaries
+  }: Props) => {
+
+  return (
+
+    <div className={`relative min-h-screen bg-gradient-to-b to-[#010511] lg:h-[140vh]`}>
+      <Head> 
+        <title>Home - Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+        
+      <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
+        <Banner netflixOriginals={netflixOriginals}/>
+        <section className='md:space-y-24'>
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My Lis Component */}
+          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
+        </section>
+        {/* Modal */}
+      </main>
+    </div>
+  )
+}
+
+export default Home
+
+
+/* Server Side Rendering, erver Side Rendering ou SSR é o processo de pegar todos os Javascript e todos os CSS de um site que, 
+geralmente é carregado no browser (client-side), e renderizá-los como estático do lado do servidor.
+
+Para fazer isso no Next.js, é necessário colocar o nome da função da seguinte forma:
+*/
+export const getServerSideProps = async () => {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ])
+
+  // JSON da API do THE MOVIE DB tem um objeto que engloba todos os dados, que é o results, por isso .results nas props.
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results
+    },
+
+  }
+};
